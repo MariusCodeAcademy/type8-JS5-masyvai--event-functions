36a68b282966300a30005882b8089d06f8a6e363
Marius <marius@example.com>
2022-02-25 12:11:20 +0200

    uzd

---
+++ Uzduotys.md
@@ -0,0 +1,30 @@
+// ----------------------Užduotys--------------------------
+// 1. Susikurkite skaičių masyva 'numbersArray' su siais nariais :
+// 2, 12, 33, -7, 28, 49, -17, 19, 42, -14, -10, 20
+
+// 2. Sekančias užduotis atlikti su naujais masyvais(kopijomis)
+// 3. Padauginti masyvo narius iš 2
+// 4. Pakelti masyvo narius kvadratu
+// 5. Padauginti masyvo narius iš jų index'0 (vietos masyve)
+// 6. Atrinkti tiktai teigimų elementų masyvą
+// 7. Atrinkti tiktai neigiamų elementų masyvą
+// 8. Atrinkti tiktai lyginių skaičių masyvą
+// 9. Atrinkti tiktai nelyginių skaičių masyvą
+// 11. Visas neigiamas vertes masyve padaryti teigiamomis
+// 12. Pakelti visas masyvo reikšmes laipsniu 'index'
+// 13. Atrinkti tik natūralių skaičių masyvą
+// 14. Suapvalinti visas masyvo reikšmes iki sveikų skaičių
+// 15. Atrinkti kas antrą elementą
+// 16. Atrinkti kas penktą elementą
+// 17. Sukurti funkciją, kuri ima masyvą ir atspausdina kiek-
+// vieną jo reikšmę atskirai: [0] => 64. (nieko negrąžina)
+// 18. Sukurti funkciją, kuri ima masyvą ir grąžina visų
+// jo elementų sumą
+// 19. Sukurti funkciją, kuri ima masyvą ir grąžina visų ele-
+// mentų vidurkį
+// 20. Sukurti funkciją, kuri ima masyvą ir grąžina didžiau-
+// sią skaičių masyve.
+// 21. Sukurti funkciją, kuri ima masyvą ir grąžina mažiau-
+// sią skaičių masyve.
+// 22. Sukurti funkciją, kuri ima masyvą ir išrikiuja jo ele-
+// mentus nuo mažiausio iki didžiausio ir grąžina tą masyvą.
--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,47 @@ let b = 5;
 let x = 11;
 let y = 7;
 
+// parasyti funkcija kuri apskaiciuoja apkritimo plota kai zinom sinduli
+// 3.14 * spinulys * spindulys
+
+function apskPlotas(spin) {
+  const plotas = Math.PI * spin ** 2;
+  console.log(`apskirtimo kurio spindulys ${spin}m., plotas yra ${plotas.toFixed(2)}kv/m`);
+  return plotas;
+}
+
+const pirmoApskPlotas = apskPlotas(14);
+console.log('pirmoApskPlotas ===', pirmoApskPlotas);
+apskPlotas(10);
+apskPlotas(56);
+
+// parasyti funkcija kuri paskaiciuoja staciakampio plota
+function staPlotas(ilgis, aukstis) {
+  const plotas = ilgis * aukstis;
+  console.log(`staciakampio kurio ilgis ${ilgis}, ploti ${aukstis} plotas yra ${plotas}kv/m`);
+}
+staPlotas(5, 10);
+staPlotas(7, 9);
+
+// parasyti funkcija kuri apskaiciuoja kambario sienu plota ir isveda atsakyma
+// i puslapio paragrafa kurio id='result'
+// kambario ilgi, ploti, auksti
+function sienuPlotas(ilgis, plotis, aukstis) {
+  const perimetras = (ilgis + plotis) * 2;
+  const sienuPlotas = perimetras * aukstis;
+  let parEl = document.getElementById('result');
+  // parEl = document.querySelector('h1');
+  parEl.innerHTML += `apskaiciuotas sienu plotas yra ${sienuPlotas} <br>`;
+}
+sienuPlotas(5, 3, 3);
+sienuPlotas(7, 4, 3);
+
+// apskaicciuoti plotus staciakampiu
+// ilgis 5
+// plotis 10
+// ilgis 7
+// plotis 9
+
 // let sum = a + b;
 // let daug = a * 2 + b * 2;
 // let visasuma = sum + daug;
@@ -26,7 +67,7 @@ function sumDaug(num1, num2) {
 }
 
 // funkcijos iskvietimas vygdomas su ()
-sumDaug(a, b);
-sumDaug(x, y);
-sumDaug(20, 30);
-sumDaug(2, 3);
+// sumDaug(a, b);
+// sumDaug(x, y);
+// sumDaug(20, 30);
+// sumDaug(2, 3);
